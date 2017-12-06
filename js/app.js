fd01ec62afb7736ff213e4da05c9ae0a078c92fe
Helvette <malastrugo@live.cl>
2017-12-06 03:36:17 -0300

    Eventos básicos, enlace con código de Materilize

--- js/app.js
+++ js/app.js
@@ -2,7 +2,22 @@ var nav = document.getElementsByTagName("nav")[0];
 var btn = document.getElementById("drop-button");
 var hideMenu = document.getElementById("hide-menu");
 
+// Función para que se vea el dropdown menú
 btn.addEventListener("click", function(){
 	hideMenu.classList.toggle("invisible");
 	hideMenu.style.transitionDelay = "2s"
+});
+
+$('.modal').modal({
+	dismissible: true, // Modal can be dismissed by clicking outside of the modal
+	opacity: .5, // Opacity of modal background
+	inDuration: 300, // Transition in duration
+	outDuration: 200, // Transition out duration
+	startingTop: '4%', // Starting top style attribute
+	endingTop: '10%', // Ending top style attribute
+});
+
+
+$(document).ready(function() {
+	$('select').material_select();
 });
